18fe182f669ac810498e63b61186715c2116c1fa
Hans Otto Wirtz <hansottowirtz@gmail.com>
2020-02-15 12:12:56 +0800

    Add pkg lock resolver, not sure if npm remote ls still works

--- plugin.js
+++ plugin.js
@@ -15,7 +15,7 @@ class ExternalsPlugin {
 
   async addExcludes() {
     const service = this.serverless.service;
-    
+
     service.package = service.package || {};
     service.package.exclude = service.package.exclude || [];
     service.package.include = service.package.include || [];
@@ -44,6 +44,8 @@ class ExternalsPlugin {
         service.package.exclude.push(`!./node_modules/${external}/${subpath}`);
       }
     });
+
+    console.log(service);
   }
 }
 
@@ -66,40 +68,15 @@ ExternalsPlugin.externals = async function(root, externals, config) {
   console.log('Listed externals:', externals.join(', '));
 
   const pkg = require(packagePath);
- 
+
   if (!externals || (!externals.length && !config.exclude)) throw new Error('No externals listed');
 
-  let allExternals = [].concat(externals);
-
-  const promises = [];
-
-  console.log(`Fetching dependencies for ${externals.length} modules...`);
-
-  remoteLs.config(config.ls || {
-    development: false,
-    optional: false,
-    peer: false
-  });
-
-  const ls = (name, version, flatten) => new Promise((res, rej) => {
-    remoteLs.ls(name, version, flatten, (result) => res(result))
-  });
-
-  const dependenciesArrays = await Promise.all(externals.map(async external => {
-    const version = pkg.dependencies[external].version || pkg.dependencies[external];
-
-    if (!version) {
-      throw new Error('External module ' + external + ' not listed in package.json dependencies');
-    }
-
-    return await ls(external, version, true);
-  }));
-
-  console.log(`Fetching done`);
-
-  dependenciesArrays.forEach(array => {
-    allExternals = allExternals.concat(array.map(s => s.split('@')[0]))
-  });
+  let allExternals;
+  if (this.options && this.options.resolver === 'npm-remote-ls') {
+    allExternals = await npmRemoteLsResolve(pkg, externals, config, packagePath);
+  } else {
+    allExternals = await packageLockResolve(pkg, externals, config, packagePath);
+  }
 
   allExternals = allExternals
     .filter(s => !!s)
@@ -116,6 +93,8 @@ ExternalsPlugin.externals = async function(root, externals, config) {
 }
 
 ExternalsPlugin.externalsWebpack = function(root, externals, config) {
+  let allExternals = [].concat(externals);
+
   const promise = ExternalsPlugin.externals(root, externals, config);
 
   return (context, query, callback) => {
@@ -136,3 +115,97 @@ ExternalsPlugin.externalsRollup = async function(root, externals, config) {
 }
 
 module.exports = ExternalsPlugin;
+
+async function packageLockResolve(pkgLock, externals, config, packagePath) {
+  let allExternals = [].concat(externals);
+
+  function getRequiredDependencies(name, stack) {
+    let dep = pkgLock.dependencies[name];
+
+    let requiredDeps = [name];
+
+    switch (typeof pkgLock.dependencies[name]) {
+      case 'object':
+        break;
+      case 'string':
+        throw new Error(`Package lock file ${packagePath} invalid`);
+      case 'undefined':
+        console.log(`External module ${name} not found, must be transitive dependency of ${stack.join(' > ')}`);
+        return [name];
+      default:
+        throw new Error(`Error occured while finding ${name} version`);
+    }
+    if (dep.dev) {
+      console.warn(name, 'is a dev dependency through', stack.join(' > '));
+    }
+
+    if (!dep.requires) return requiredDeps;
+    requiredDeps = Object.keys(dep.requires).reduce((acc, cur) => {
+      return [...acc, ...getRequiredDependencies(cur, [...stack, name])];
+    }, requiredDeps);
+    return requiredDeps;
+  }
+
+  const allRequiredDependencies = externals.reduce((acc, cur) => {
+    let dep = pkgLock.dependencies[cur];
+    switch (typeof dep) {
+      case 'object':
+        break;
+      case 'string':
+        throw new Error(`Package lock file ${packagePath} invalid`);
+      case 'undefined':
+        throw new Error(`External module ${cur} not listed in ${packagePath} dependencies`);
+      default:
+        throw new Error(`Error occured while finding ${cur} version`);
+    }
+    if (!dep.requires) return acc;
+    const rds = getRequiredDependencies(cur, [pkgLock.name || 'root']);
+    return [...acc, ...rds];
+  }, []);
+
+  allExternals = [...externals, ...allRequiredDependencies];
+
+  return allExternals;
+}
+
+async function npmRemoteLsResolve(pkg, externals, config, packagePath) {
+  let allExternals = [...externals];
+
+  console.log(`Fetching dependencies for ${externals.length} modules...`);
+
+  remoteLs.config(config.ls || {
+    development: false,
+    optional: false,
+    peer: false
+  });
+
+  const ls = (name, version, flatten) => new Promise((res, rej) => {
+    remoteLs.ls(name, version, flatten, (result) => res(result))
+  });
+
+  const dependenciesArrays = await Promise.all(externals.map(async external => {
+    let version;
+    switch (typeof pkg.dependencies[external]) {
+      case 'string':
+        version = pkg.dependencies[external];
+        break;
+      case 'object':
+        version = pkg.dependencies[external].version;
+        break;
+      case 'undefined':
+        throw new Error('External module ' + external + ` not listed in ${packagePath} dependencies`);
+      default:
+        throw new Error('Error occured while finding ' + external + ' version');
+    }
+
+    return await ls(external, version, true);
+  }));
+
+  console.log(`Fetching done`);
+
+  dependenciesArrays.forEach(array => {
+    allExternals = allExternals.concat(array.map(s => s.split('@')[0]));
+  });
+
+  return allExternals;
+}
